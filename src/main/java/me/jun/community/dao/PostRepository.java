678dcf54af0bb7fc0f36e1d47542591d40718637
kjh9267 <junhokim9267@kakao.com>
2020-02-24 21:16:10 +0900

    refactor domain

--- src/main/java/me/jun/community/dao/PostRepository.java
+++ src/main/java/me/jun/community/dao/PostRepository.java
@@ -1,5 +1,6 @@
-package me.jun.community.post;
+package me.jun.community.dao;
 
+import me.jun.community.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
