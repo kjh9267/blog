26a12eb72f629aa1aa0e68ae008572882a5335f5
kjh9267 <junhokim9267@kakao.com>
2020-02-26 19:38:59 +0900

    refactor package

--- src/main/java/me/jun/community/dao/CommentRepository.java
+++ src/main/java/me/jun/community/dao/CommentRepository.java
@@ -1,6 +1,6 @@
 package me.jun.community.dao;
 
-import me.jun.community.domain.Comment;
+import me.jun.community.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
