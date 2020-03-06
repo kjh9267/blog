824a806ef70bac5190b606b4967f5e7dcd415cd5
kjh9267 <intellijunho@kakao.com>
2020-03-06 15:38:58 +0900

    add 'post read dto'.

---
+++ src/main/java/me/jun/guestbook/dto/PostReadDto.java
@@ -0,0 +1,26 @@
+package me.jun.guestbook.dto;
+
+import lombok.Getter;
+import me.jun.guestbook.domain.post.Post;
+
+import java.util.Date;
+
+@Getter
+public class PostReadDto {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String content;
+    private Date date;
+    private String password;
+
+    public PostReadDto(Post post) {
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.author = post.getAuthor();
+        this.content = post.getContent();
+        this.date = post.getDate();
+        this.password = post.getPassword();
+    }
+}
