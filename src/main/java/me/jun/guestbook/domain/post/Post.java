6b0f18531f81380fbff420cea0c06bd64f3e444d
kjh9267 <intellijunho@kakao.com>
2020-03-06 14:56:24 +0900

    create domain post.

---
+++ src/main/java/me/jun/guestbook/domain/post/Post.java
@@ -0,0 +1,34 @@
+package me.jun.guestbooktemp.domain.post;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String author;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String password;
+
+}
