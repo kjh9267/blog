6adc7504f094436974ef8c145f780f6d401e221a
kjh9267 <intellijunho@kakao.com>
2020-04-29 12:30:22 +0900

    Modify Service Class.

--- src/main/java/me/jun/guestbook/service/PostService.java
+++ src/main/java/me/jun/guestbook/service/PostService.java
@@ -1,43 +1,15 @@
 package me.jun.guestbook.service;
 
-import me.jun.guestbook.domain.post.Post;
-import me.jun.guestbook.domain.post.PostRepository;
 import me.jun.guestbook.dto.PostReadDto;
 import me.jun.guestbook.dto.PostSaveDto;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class PostService {
+public interface PostService {
 
-    @Autowired
-    PostRepository postRepository;
+    List<PostReadDto> getPost();
 
-    public List<PostReadDto> getList() {
-        List<Post> posts = postRepository.findAll();
-        List<PostReadDto> list = new ArrayList<>();
+    void savePost(PostSaveDto postSaveDto);
 
-        for(Post post: posts) {
-            list.add(new PostReadDto(post));
-        }
-        return list;
-    }
-
-    public void savePost(PostSaveDto dto) {
-        Post entity = dto.toEntity();
-        postRepository.save(entity);
-    }
-
-    public void deletePost(Long requestId, String requestPassword) {
-        Optional<Post> entity = postRepository.findById(requestId);
-        String password = entity.get().getPassword();
-
-        if(requestPassword.equals(password)) {
-            postRepository.delete(entity.get());
-        }
-    }
+    void deletePost(Long id, String password);
 }
