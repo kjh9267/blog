bbe3b435f0d79ac67e310f7f3f41b9da81860fa0
kjh9267 <intellijunho@kakao.com>
2020-09-10 22:37:40 +0900

    feat: Create Post Class -feat: Add relationship Post and Account

---
+++ src/main/java/me/jun/guestbook/domain/Account.java
@@ -0,0 +1,33 @@
+package me.jun.guestbook.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode(of = "id")
+public class Account {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 12, nullable = false)
+    private String name;
+
+    @Column(length = 20, nullable = false)
+    private String email;
+
+    @Column(length = 12, nullable = false)
+    private String password;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "account")
+    private Set<Post> posts = new HashSet<>();
+}
