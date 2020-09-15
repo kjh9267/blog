f72afd55e6d4581647184dd65d5d16fbb77e54d0
kjh9267 <junhokim9267@kakao.com>
2020-09-15 21:35:32 +0900

    -feat: Create RequestPostId dto

---
+++ src/main/java/me/jun/guestbook/dto/RequestPostId.java
@@ -0,0 +1,13 @@
+package me.jun.guestbook.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@Getter
+public class RequestPostId {
+
+    private final Long id;
+}
