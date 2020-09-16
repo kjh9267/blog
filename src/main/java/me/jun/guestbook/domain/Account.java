81caae39fbda97992ea4e9fde803d3fd3f5c608b
kjh9267 <kjh9267z@naver.com>
2020-09-16 19:23:46 +0900

    feat: Create getAccount method -feat: Create AccountLoginDto -feat: Create AccountInfoDto

--- src/main/java/me/jun/guestbook/domain/Account.java
+++ src/main/java/me/jun/guestbook/domain/Account.java
@@ -21,7 +21,7 @@ public class Account {
     @Column(length = 12, nullable = false)
     private String name;
 
-    @Column(length = 20, nullable = false)
+    @Column(unique = true, length = 20, nullable = false)
     private String email;
 
     @Column(length = 12, nullable = false)
