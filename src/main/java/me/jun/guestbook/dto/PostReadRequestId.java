3650cded983b91c2aca8e8dfed18bd32af02c732
kjh9267 <intellijunho@naver.com>
2020-09-16 17:59:04 +0900

    refactor: Refactor ReadPost method's return type to PostReadDto

--- src/main/java/me/jun/guestbook/dto/PostReadRequestId.java
+++ src/main/java/me/jun/guestbook/dto/PostReadRequestId.java
@@ -1,22 +1,13 @@
 package me.jun.guestbook.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import me.jun.guestbook.domain.Account;
 
 @NoArgsConstructor(force = true)
 @AllArgsConstructor
 @Getter
-@Builder
-public class PostReadDto {
+public class PostReadRequestId {
 
     private final Long id;
-
-    private final String title;
-
-    private final String content;
-
-    private final Account account;
 }
