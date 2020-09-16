26f34f1e4700c06dae93cf42b3f9886c12c9d944
kjh9267 <junhokim9267@kakao.com>
2020-09-16 20:22:11 +0900

    feat: Create deleteAccount Method

--- src/main/java/me/jun/guestbook/service/AccountService.java
+++ src/main/java/me/jun/guestbook/service/AccountService.java
@@ -3,13 +3,11 @@ package me.jun.guestbook.service;
 import me.jun.guestbook.dao.AccountRepository;
 import me.jun.guestbook.domain.Account;
 import me.jun.guestbook.dto.AccountInfoDto;
-import me.jun.guestbook.dto.AccountLoginDto;
+import me.jun.guestbook.dto.AccountRequestDto;
 import me.jun.guestbook.dto.AccountRegisterDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
-
-import java.sql.SQLException;
 
 @Service
 public class AccountService {
@@ -17,20 +15,26 @@ public class AccountService {
     @Autowired
     AccountRepository accountRepository;
 
-    public AccountInfoDto getAccount(AccountLoginDto accountLoginDto) {
-        final String requestEmail = accountLoginDto.getEmail();
-
-        final Account account = accountRepository.findByEmail(requestEmail)
-                .orElseThrow(IllegalArgumentException::new);
-
-        if (!accountLoginDto.getPassword().equals(account.getPassword())) {
-            throw new IllegalArgumentException("wrong password");
-        }
+    public AccountInfoDto getAccount(AccountRequestDto accountRequestDto) {
+        final Account account = getAccountFromRepository(accountRequestDto);
 
         return AccountInfoDto.builder()
                 .name(account.getName())
                 .email(account.getEmail())
+                .posts(account.getPosts())
                 .build();
+    }
+
+    private Account getAccountFromRepository(AccountRequestDto accountRequestDto) {
+        final String requestEmail = accountRequestDto.getEmail();
+
+        final Account account = accountRepository.findByEmail(requestEmail)
+                .orElseThrow(IllegalArgumentException::new);
+
+        if (!accountRequestDto.getPassword().equals(account.getPassword())) {
+            throw new IllegalArgumentException("wrong password");
+        }
+        return account;
     }
 
     public AccountInfoDto createAccount(AccountRegisterDto accountRegisterDto) {
@@ -52,5 +56,11 @@ public class AccountService {
         catch (DataIntegrityViolationException e) {
             throw new IllegalArgumentException("invalid email");
         }
+    }
+
+    public void deleteAccount(AccountRequestDto accountRequestDto) {
+        final Account account = getAccountFromRepository(accountRequestDto);
+
+        accountRepository.deleteById(account.getId());
     }
 }
