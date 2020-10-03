65a2b196c5bf3e8b4941c76dc878823487e42e91
kjh9267 <intellijunho@naver.com>
2020-10-03 17:27:03 +0900

    feat: Create PostController -feat: Create createPost method

---
+++ src/main/java/me/jun/guestbook/common/Runner.java
@@ -0,0 +1,26 @@
+package me.jun.guestbook.common;
+
+import me.jun.guestbook.dto.AccountRequestDto;
+import me.jun.guestbook.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements ApplicationRunner {
+
+    @Autowired
+    AccountService accountService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        accountService.createAccount(
+                AccountRequestDto.builder()
+                        .email("user@email.com")
+                        .password("pass")
+                        .name("jun")
+                        .build()
+        );
+    }
+}
