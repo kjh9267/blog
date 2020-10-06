73d04d7c14e8dac209afd4d016e6063f6aa6c2e7
kjh9267 <intellijunho@naver.com>
2020-10-06 19:53:30 +0900

    DAO Logging

--- src/main/java/me/jun/guestbook/domain/post/TempPostRepository.java
+++
@@ -1,6 +0,0 @@
-package me.jun.guestbook.domain.post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TempPostRepository extends JpaRepository<TempPost, Long> {
-}
