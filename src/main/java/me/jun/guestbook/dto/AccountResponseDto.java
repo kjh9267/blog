d03254b73777a4433873cfb4a607f26dfdb84438
kjh9267 <junhokim9267@kakao.com>
2020-10-06 18:31:21 +0900

    feat: Create login and session

--- src/main/java/me/jun/guestbook/dto/AccountResponseDto.java
+++ src/main/java/me/jun/guestbook/dto/AccountResponseDto.java
@@ -1,31 +1,23 @@
 package me.jun.guestbook.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import me.jun.guestbook.domain.Account;
-import me.jun.guestbook.domain.Post;
-
-import java.util.List;
 
 @NoArgsConstructor(force = true)
 @AllArgsConstructor
 @Getter
 @Builder
+@ToString
 public class AccountResponseDto {
 
     private final String name;
 
     private final String email;
 
-    private final List<Post> posts;
-
     public static AccountResponseDto from(Account account) {
         return AccountResponseDto.builder()
                 .name(account.getName())
                 .email(account.getEmail())
-                .posts(account.getPosts())
                 .build();
     }
 }
