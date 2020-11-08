2960d2eaf8e81a990b4fcf083ab0a3645bb857cc
kjh9267 <intellijunho@naver.com>
2020-11-08 20:06:46 +0900

    delete

--- community/src/main/java/me/jun/community/dao/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package me.jun.community.dao;
-
-import me.jun.community.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
