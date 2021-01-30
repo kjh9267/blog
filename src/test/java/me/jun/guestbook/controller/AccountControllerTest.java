0600ad1c5dcf413bbd2b860cf8a332f7275092b4
kjh9267 <intellijunho@kakao.com>
2021-01-30 18:27:30 +0900

    refactor: http session test

--- src/test/java/me/jun/guestbook/controller/AccountControllerTest.java
+++ src/test/java/me/jun/guestbook/controller/AccountControllerTest.java
@@ -1,5 +1,6 @@
 package me.jun.guestbook.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.jun.guestbook.dto.AccountRequestDto;
 import me.jun.guestbook.service.AccountService;
@@ -8,10 +9,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,6 +33,8 @@ public class AccountControllerTest {
 
     @Autowired
     AccountService accountService;
+
+    MockHttpSession mockHttpSession;
 
     @Test
     public void registerTest() throws Exception {
@@ -63,6 +68,27 @@ public class AccountControllerTest {
         mockMvc.perform(post("/login")
                     .content(content)
                     .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    public void loginSessionTest() throws Exception {
+        final AccountRequestDto requestDto = AccountRequestDto.builder()
+                .name("jun")
+                .email("testuser@email.com")
+                .password("pass")
+                .build();
+
+        accountService.createAccount(requestDto);
+
+        mockHttpSession = new MockHttpSession();
+
+        mockHttpSession.setAttribute("login", requestDto);
+
+        mockMvc.perform(get("/index")
+                .session(mockHttpSession)
+                .contentType("application/json"))
                 .andDo(print())
                 .andExpect(status().is3xxRedirection());
     }
