c61965d8f57e23f4534eea855cbd84d0339bf285
kjh9267 <junhokim9267@kakao.com>
2021-01-30 18:27:30 +0900

    refactor: http session test

--- src/main/java/me/jun/guestbook/config/AuthConfig.java
+++ src/main/java/me/jun/guestbook/config/AuthConfig.java
@@ -17,7 +17,6 @@ public class AuthConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interceptor)
-                .excludePathPatterns("/login")
-                .excludePathPatterns("/**");
+                .addPathPatterns("/**");
     }
 }
