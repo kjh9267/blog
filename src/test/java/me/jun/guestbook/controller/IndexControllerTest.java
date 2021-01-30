90cdb2a19300fb61eeb534f3ffb80c241c454ec3
kjh9267 <intellijunho@kakao.com>
2021-01-30 19:07:23 +0900

    refactor: add mock http session for authentication.

--- src/test/java/me/jun/guestbook/controller/IndexControllerTest.java
+++ src/test/java/me/jun/guestbook/controller/IndexControllerTest.java
@@ -1,18 +1,21 @@
 package me.jun.guestbook.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.jun.guestbook.dto.AccountRequestDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,11 +29,22 @@ public class IndexControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    MockHttpSession mockHttpSession;
+
     @Test
     public void indexTest() throws Exception {
-        final MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/index");
+        AccountRequestDto accountRequestDto = AccountRequestDto.builder()
+                .name("testuser")
+                .password("pass")
+                .email("testuser@email.com")
+                .build();
 
-        mockMvc.perform(request)
+        mockHttpSession = new MockHttpSession();
+
+        mockHttpSession.setAttribute("login", accountRequestDto);
+
+        mockMvc.perform(get("/index")
+                    .session(mockHttpSession))
                 .andDo(print())
                 .andExpect(status().is3xxRedirection());
     }
