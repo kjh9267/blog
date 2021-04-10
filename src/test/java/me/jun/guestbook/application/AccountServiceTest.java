69bb74e3791442b87fca54ff5c569febbe85158b
kjh9267 <intellijunho@kakao.com>
2021-04-11 02:10:46 +0900

    test: Use mock object for unit testing

--- src/test/java/me/jun/guestbook/application/AccountServiceTest.java
+++ src/test/java/me/jun/guestbook/application/AccountServiceTest.java
@@ -1,137 +1,109 @@
 package me.jun.guestbook.application;
 
-import me.jun.guestbook.dto.AccountRequestDto;
-import me.jun.guestbook.dto.AccountResponseDto;
 import me.jun.guestbook.application.exception.DuplicatedEmailException;
 import me.jun.guestbook.application.exception.EmailNotFoundException;
 import me.jun.guestbook.domain.account.Account;
 import me.jun.guestbook.domain.account.AccountRepository;
-import me.jun.guestbook.domain.account.WrongPasswordException;
-import org.junit.Before;
+import me.jun.guestbook.dto.AccountRequest;
+import me.jun.guestbook.dto.AccountResponse;
 import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@ExtendWith(MockitoExtension.class)
 public class AccountServiceTest {
 
-    @Autowired
-    AccountService accountService;
+    private AccountService accountService;
 
-    @Autowired
-    AccountRepository accountRepository;
+    @Mock
+    private AccountRepository accountRepository;
 
     private Account account;
 
-    String password = "pass";
+    private AccountRequest accountRequest;
 
-    @Before
+    private final String name = "testuser";
+
+    private final String email = "testuser@email.com";
+
+    private final String password = "pass";
+
+    @BeforeEach
     public void setUp() {
+        accountService = new AccountService(accountRepository);
+
         account = Account.builder()
-                .name("jun")
-                .email("testuser@email.com")
+                .name(name)
+                .email(email)
+                .password(password)
+                .build();
+
+        accountRequest = AccountRequest.builder()
+                .name(name)
+                .email(email)
                 .password(password)
                 .build();
     }
 
     @Test
-    public void readAccountTest() {
-        // Given
-        accountRepository.save(account);
-
-        final AccountRequestDto accountRequestDto = AccountRequestDto.builder()
-                .email("testuser@email.com")
-                .password(password)
-                .build();
+    void loginTest() {
+        given(accountRepository.findByEmail(email)).willReturn(Optional.of(account));
 
         // When
-        final AccountResponseDto accountResponseDto = accountService.login(accountRequestDto);
+        AccountResponse accountResponse = accountService.login(accountRequest);
 
         // Then
-        assertThat(accountResponseDto).isEqualToComparingOnlyGivenFields(accountRequestDto, "email");
+        assertAll(
+                () -> assertThat(accountResponse).isInstanceOf(AccountResponse.class),
+                () -> assertThat(accountResponse.getName()).isEqualTo(name),
+                () -> assertThat(accountResponse.getEmail()).isEqualTo(email)
+        );
+
     }
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
 
     @Test
-    public void getAccountFailTest() {
-        // Expected
-        expectedException.expect(WrongPasswordException.class);
-        expectedException.expectMessage("wrong password");
+    void NoAccount_loginFailTest() {
+        given(accountRepository.findByEmail(email)).willReturn(Optional.empty());
 
-        // Given
-        accountRepository.save(account);
-
-        final AccountRequestDto accountRequestDto = AccountRequestDto.builder()
-                .email("testuser@email.com")
-                .password("abc")
-                .build();
-
-        // When
-        accountService.login(accountRequestDto);
+        assertThrows(EmailNotFoundException.class,
+                () -> accountService.login(accountRequest)
+        );
     }
 
     @Test
-    public void createAccountTest() {
-        // Given
-        accountRepository.save(account);
+    void registerTest() {
+        given(accountRepository.save(account)).willReturn(account);
 
-        final AccountRequestDto accountRequestDto = AccountRequestDto.builder()
-                .name(account.getName())
-                .password(password)
-                .email("new_user@email.com")
-                .build();
+        AccountResponse accountResponse = accountService.register(accountRequest);
 
-        // When
-        final AccountResponseDto accountResponseDto = accountService.createAccount(accountRequestDto);
-
-        // Then
-        assertThat(accountResponseDto.getEmail()).isEqualTo(accountRequestDto.getEmail());
-        assertThat(accountResponseDto.getName()).isEqualTo(accountRequestDto.getName());
+        assertAll(
+                () -> assertThat(accountResponse).isInstanceOf(AccountResponse.class),
+                () -> assertThat(accountResponse.getName()).isEqualTo(name),
+                () -> assertThat(accountResponse.getEmail()).isEqualTo(email)
+        );
     }
 
     @Test
-    public void createAccountFailTest() {
-        // Expected
-        expectedException.expect(DuplicatedEmailException.class);
-        expectedException.expectMessage("Email already exists");
+    void registerFailTest() {
+        given(accountRepository.save(account)).willThrow(new DataIntegrityViolationException(""));
 
-        // Given
-        accountRepository.save(account);
-
-        final AccountRequestDto accountRegisterDto = AccountRequestDto.builder()
-                .name(account.getName())
-                .password("pass")
-                .email(account.getEmail())
-                .build();
-
-        // When
-        accountService.createAccount(accountRegisterDto);
-    }
-
-    @Test
-    public void deleteAccountTest() {
-        // Expected
-        expectedException.expect(EmailNotFoundException.class);
-
-        // Given
-        final AccountRequestDto accountRequestDto = AccountRequestDto.builder()
-                .email(account.getEmail())
-                .password(password)
-                .build();
-
-        // When
-        accountService.deleteAccount(accountRequestDto);
-        accountService.login(accountRequestDto);
+        assertThrows(DuplicatedEmailException.class,
+                () -> accountService.register(accountRequest)
+        );
     }
 }
