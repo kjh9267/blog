2ad3802c348f7c88c79758c7de2d3b2a2de6c5ae
kjh9267 <junhokim9267@kakao.com>
2021-05-29 19:02:41 +0900

    test: Read non-existent post - feat: Add post not found exception handler

--- src/main/java/me/jun/guestbook/ui/GlobalExceptionHandler.java
+++ src/main/java/me/jun/guestbook/ui/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package me.jun.guestbook.ui;
 
+import me.jun.guestbook.post.application.PostNotFoundException;
 import me.jun.guestbook.ui.error.ErrorCode;
 import me.jun.guestbook.ui.error.ErrorResponse;
 import org.slf4j.Logger;
@@ -7,17 +8,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BindException.class)
-    protected ResponseEntity<ErrorResponse> bindExceptionHandler(BindException e) {
+    public ResponseEntity<ErrorResponse> bindExceptionHandler(BindException e) {
         Logger logger = LoggerFactory.getLogger("empty input error logger");
         logger.info("empty input");
         ErrorResponse errorResponse = ErrorResponse.from(ErrorCode.EMPTY_VALUE);
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(PostNotFoundException.class)
+    public ResponseEntity<ErrorResponse> postNotFoundExceptionHandler(Exception e) {
+        ErrorResponse errorResponse = ErrorResponse.from(ErrorCode.POST_NOT_FOUND);
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorResponse.getStatusCode()));
+    }
+
 }
--- src/main/java/me/jun/guestbook/ui/error/ErrorCode.java
+++ src/main/java/me/jun/guestbook/ui/error/ErrorCode.java
@@ -1,11 +1,13 @@
 package me.jun.guestbook.ui.error;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
 
-    EMPTY_VALUE(400, "Empty input");
+    EMPTY_VALUE(400, "Empty input"),
+    POST_NOT_FOUND(404, "No post");
 
     private final int statusCode;
     private final String message;
