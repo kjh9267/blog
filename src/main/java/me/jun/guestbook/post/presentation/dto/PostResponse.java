a323c3075a82064cbcead3b9f100c3bd319739ef
kjh9267 <junhokim9267@kakao.com>
2021-06-17 23:34:13 +0900

    refactor: Change response of post api

--- src/main/java/me/jun/guestbook/post/presentation/dto/PostResponse.java
+++ src/main/java/me/jun/guestbook/post/presentation/dto/PostResponse.java
@@ -1,5 +1,7 @@
 package me.jun.guestbook.post.presentation.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,11 +12,10 @@ import me.jun.guestbook.post.domain.Post;
 @AllArgsConstructor
 @Getter
 @Builder
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class PostResponse {
 
     private final Long id;
-
-    private final String writer;
 
     private final String title;
 
