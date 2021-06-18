b9719a736c4cff9034764c9187f3494fe33ba689
kjh9267 <intellijunho@naver.com>
2021-06-18 17:25:13 +0900

    feat: Create comment structure

---
+++ src/main/java/me/jun/guestbook/comment/presentation/CommentController.java
@@ -0,0 +1,7 @@
+package me.jun.guestbook.comment.presentation;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "/api/comment")
+public class CommentController {
+}
