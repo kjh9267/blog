a9a973a68175d88350793fb6a8dddfaf2017a71c
kjh9267 <kjh9267z@naver.com>
2021-06-18 19:37:55 +0900

    feat: Create createComment method

---
+++ src/main/java/me/jun/guestbook/comment/presentation/dto/CommentCreateRequest.java
@@ -0,0 +1,22 @@
+package me.jun.guestbook.comment.presentation.dto;
+
+import lombok.*;
+import me.jun.guestbook.comment.domain.Comment;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+public class CommentCreateRequest {
+
+    private Long postId;
+
+    private String content;
+
+    public Comment toEntity() {
+        return Comment.builder()
+                .postId(postId)
+                .content(content)
+                .build();
+    }
+}
