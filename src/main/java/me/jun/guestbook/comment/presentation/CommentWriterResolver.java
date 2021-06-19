e6d51764e8e73b58c8dff5e19d2c0073d984f1c1
kjh9267 <intellijunho@naver.com>
2021-06-20 02:43:32 +0900

    feat: Create abstract jwt provider

--- src/main/java/me/jun/guestbook/comment/presentation/CommentWriterResolver.java
+++ src/main/java/me/jun/guestbook/comment/presentation/CommentWriterResolver.java
@@ -1,48 +1,23 @@
 package me.jun.guestbook.comment.presentation;
 
-import lombok.RequiredArgsConstructor;
 import me.jun.guestbook.comment.application.CommentWriterService;
-import me.jun.guestbook.security.InvalidTokenException;
+import me.jun.guestbook.comment.presentation.dto.CommentWriterInfo;
+import me.jun.guestbook.security.AbstractWriterResolver;
 import me.jun.guestbook.security.JwtProvider;
-import org.springframework.core.MethodParameter;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.support.WebDataBinderFactory;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.ModelAndViewContainer;
-
-import java.util.Optional;
 
 @Component
-@RequiredArgsConstructor
-public class CommentWriterResolver implements HandlerMethodArgumentResolver {
-
-    private final JwtProvider provider;
+public class CommentWriterResolver extends AbstractWriterResolver<CommentWriterInfo> {
 
     private final CommentWriterService commentWriterService;
 
-    @Override
-    public boolean supportsParameter(MethodParameter methodParameter) {
-        return methodParameter.hasParameterAnnotation(CommentWriter.class);
+    public CommentWriterResolver(CommentWriterService commentWriterService, JwtProvider provider) {
+        super(provider);
+        this.commentWriterService = commentWriterService;
     }
 
     @Override
-    public Object resolveArgument(MethodParameter methodParameter,
-                                  ModelAndViewContainer modelAndViewContainer,
-                                  NativeWebRequest nativeWebRequest,
-                                  WebDataBinderFactory webDataBinderFactory)
-            throws Exception {
-        String token = extractToken(nativeWebRequest)
-                .orElseThrow(() -> new InvalidTokenException("No token"));
-
-        provider.validateToken(token);
-        String email = provider.extractSubject(token);
+    protected CommentWriterInfo retrieveWriterBy(String email) {
         return commentWriterService.retrieveCommentWriterBy(email);
-    }
-
-    private Optional<String> extractToken(NativeWebRequest nativeWebRequest) {
-        return Optional.ofNullable(nativeWebRequest
-                .getHeader(HttpHeaders.AUTHORIZATION));
     }
 }
