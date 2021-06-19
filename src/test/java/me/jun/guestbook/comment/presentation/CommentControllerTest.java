9769fd1131a6cd733df2746a2c273516ea506a77
kjh9267 <intellijunho@kakao.com>
2021-06-20 01:12:46 +0900

    feat: Create createPost handler

--- src/test/java/me/jun/guestbook/comment/presentation/CommentControllerTest.java
+++ src/test/java/me/jun/guestbook/comment/presentation/CommentControllerTest.java
@@ -1,6 +1,5 @@
 package me.jun.guestbook.comment.presentation;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.jun.guestbook.comment.application.CommentService;
 import me.jun.guestbook.comment.presentation.dto.CommentCreateRequest;
@@ -14,16 +13,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
-@Ignore
 public class CommentControllerTest {
     private static final String HAL_JSON = "application/hal+json";
     private static final String JSON = "application/json";
@@ -40,12 +42,16 @@ public class CommentControllerTest {
     @Autowired
     private JwtProvider jwtProvider;
 
+    private String jwt;
+
     private CommentCreateRequest commentCreateRequest;
 
     private CommentResponse commentResponse;
 
     @BeforeEach
     void setUp() {
+        jwt = jwtProvider.createJwt("testuser@email.com");
+
         commentResponse = CommentResponse.builder()
                 .id(1L)
                 .writerId(1L)
@@ -56,17 +62,27 @@ public class CommentControllerTest {
 
     @Test
     void createCommentTest() throws Exception {
-        CommentCreateRequest commentCreateRequest = this.commentCreateRequest.builder()
+        commentCreateRequest = CommentCreateRequest.builder()
                 .postId(1L)
                 .content("test content")
                 .build();
 
         String content = objectMapper.writeValueAsString(commentCreateRequest);
 
-        mockMvc.perform(post("/api/post")
-                    .content(content)
-                    .contentType(JSON)
-                    .accept(HAL_JSON))
-                .andDo(print());
+        given(commentService.createComment(any(), any()))
+                .willReturn(commentResponse);
+
+        mockMvc.perform(post("/api/comment")
+                        .content(content)
+                        .contentType(JSON)
+                        .accept(HAL_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, jwt))
+                .andDo(print())
+                .andExpect(header().string("location", "http://localhost/api/comment/1"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("_links.self.href").value("http://localhost/api/comment/1"))
+                .andExpect(jsonPath("_links.get_comment.href").value("http://localhost/api/comment/1"))
+                .andExpect(jsonPath("_links.update_comment.href").value("http://localhost/api/comment/1"))
+                .andExpect(jsonPath("_links.delete_comment.href").value("http://localhost/api/comment/1"));
     }
 }
