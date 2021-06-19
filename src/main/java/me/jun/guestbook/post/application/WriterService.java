fdc670f8b7d38bab58acdeace49d0362f31ee304
kjh9267 <intellijunho@kakao.com>
2021-06-19 23:59:10 +0900

    refactor: Seperate Resolver by context

--- src/main/java/me/jun/guestbook/post/application/WriterService.java
+++
@@ -1,7 +0,0 @@
-package me.jun.guestbook.post.application;
-
-import me.jun.guestbook.post.presentation.dto.WriterInfo;
-
-public interface WriterService {
-    WriterInfo findWriterByEmail(String email);
-}
---
+++ src/main/java/me/jun/guestbook/post/infra/PostWriterServiceImpl.java
@@ -0,0 +1,21 @@
+package me.jun.guestbook.post.infra;
+
+import lombok.RequiredArgsConstructor;
+import me.jun.guestbook.guest.application.GuestService;
+import me.jun.guestbook.guest.presentation.dto.GuestResponse;
+import me.jun.guestbook.post.application.PostWriterService;
+import me.jun.guestbook.post.presentation.dto.PostWriterInfo;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostWriterServiceImpl implements PostWriterService {
+
+    private final GuestService guestService;
+
+    @Override
+    public PostWriterInfo retrievePostWriterBy(String email) {
+        GuestResponse guestResponse = guestService.retrieveGuestBy(email);
+        return PostWriterInfo.from(guestResponse);
+    }
+}
