9e17d7292582db9a06f20fdf56e34502a4aebb5b
kjh9267 <intellijunho@kakao.com>
2021-06-20 08:24:33 +0900

    test: Create constant of links

---
+++ src/test/java/me/jun/guestbook/utils/ControllerTestUtils.java
@@ -0,0 +1,31 @@
+package me.jun.guestbook.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public abstract class ControllerTestUtils {
+
+    public static final String LINKS_SELF_HREF = "_links.self.href";
+
+    public static final String LINKS_CREATE_POST_HREF = "_links.create_post.href";
+
+    public static final String LINKS_GET_POST_HREF = "_links.get_post.href";
+
+    public static final String LINKS_UPDATE_POST_HREF = "_links.update_post.href";
+
+    public static final String LINKS_DELETE_POST_HREF = "_links.delete_post.href";
+
+    public static final String LINKS_CREATE_COMMENT_HREF = "_links.create_comment.href";
+
+    public static final String LINKS_GET_COMMENT_HREF = "_links.get_comment.href";
+
+    public static final String LINKS_UPDATE_COMMENT_HREF = "_links.update_comment.href";
+
+    public static final String LINKS_DELETE_COMMENT_HREF = "_links.delete_comment.href";
+
+    public static final String POSTS_SELF_URI = "http://localhost/api/posts";
+
+    public static final String COMMENTS_SELF_URI = "http://localhost/api/comments";
+
+}
