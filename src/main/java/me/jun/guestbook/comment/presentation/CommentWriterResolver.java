a08208aee2beda2cb520b942e5d93916cfd5810b
kjh9267 <junhokim9267@kakao.com>
2021-06-21 16:32:10 +0900

    test: Create writer service mockbean -feat: Create read, update, delete comment controller -test: Create read, update, delete comment controller test

--- src/main/java/me/jun/guestbook/comment/presentation/CommentWriterResolver.java
+++ src/main/java/me/jun/guestbook/comment/presentation/CommentWriterResolver.java
@@ -1,23 +1,53 @@
-package me.jun.guestbook.comment.presentation;
+package me.jun.guestbook.post.presentation;
 
 import me.jun.guestbook.comment.application.CommentWriterService;
-import me.jun.guestbook.comment.presentation.dto.CommentWriterInfo;
-import me.jun.guestbook.security.AbstractWriterResolver;
+import me.jun.guestbook.comment.domain.Comment;
+import me.jun.guestbook.comment.presentation.CommentWriter;
+import me.jun.guestbook.post.application.PostWriterService;
+import me.jun.guestbook.security.InvalidTokenException;
 import me.jun.guestbook.security.JwtProvider;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.Optional;
 
 @Component
-public class CommentWriterResolver extends AbstractWriterResolver<CommentWriterInfo> {
+public class CommentWriterResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtProvider provider;
 
     private final CommentWriterService commentWriterService;
 
-    public CommentWriterResolver(CommentWriterService commentWriterService, JwtProvider provider) {
-        super(provider);
+    public CommentWriterResolver(JwtProvider provider, CommentWriterService commentWriterService) {
+        this.provider = provider;
         this.commentWriterService = commentWriterService;
     }
 
     @Override
-    protected CommentWriterInfo retrieveWriterBy(String email) {
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        return methodParameter.hasParameterAnnotation(CommentWriter.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter,
+                                  ModelAndViewContainer modelAndViewContainer,
+                                  NativeWebRequest nativeWebRequest,
+                                  WebDataBinderFactory webDataBinderFactory) {
+        String token = extractToken(nativeWebRequest)
+                .orElseThrow(() -> new InvalidTokenException("No token"));
+
+        provider.validateToken(token);
+        String email = provider.extractSubject(token);
         return commentWriterService.retrieveCommentWriterBy(email);
+    }
+
+    private Optional<String> extractToken(NativeWebRequest nativeWebRequest) {
+        return Optional.ofNullable(nativeWebRequest
+                .getHeader(HttpHeaders.AUTHORIZATION));
     }
 }
--- src/main/java/me/jun/guestbook/post/presentation/PostWriterResolver.java
+++ src/main/java/me/jun/guestbook/post/presentation/PostWriterResolver.java
@@ -1,23 +1,51 @@
 package me.jun.guestbook.post.presentation;
 
 import me.jun.guestbook.post.application.PostWriterService;
-import me.jun.guestbook.post.presentation.dto.PostWriterInfo;
-import me.jun.guestbook.security.AbstractWriterResolver;
+import me.jun.guestbook.post.presentation.PostWriter;
+import me.jun.guestbook.security.InvalidTokenException;
 import me.jun.guestbook.security.JwtProvider;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.Optional;
 
 @Component
-public class PostWriterResolver extends AbstractWriterResolver<PostWriterInfo> {
+public class PostWriterResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtProvider provider;
 
     private final PostWriterService postWriterService;
 
-    public PostWriterResolver(PostWriterService postWriterService, JwtProvider provider) {
-        super(provider);
+    public PostWriterResolver(JwtProvider provider, PostWriterService postWriterService) {
+        this.provider = provider;
         this.postWriterService = postWriterService;
     }
 
     @Override
-    protected PostWriterInfo retrieveWriterBy(String email) {
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        return methodParameter.hasParameterAnnotation(PostWriter.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter,
+                                  ModelAndViewContainer modelAndViewContainer,
+                                  NativeWebRequest nativeWebRequest,
+                                  WebDataBinderFactory webDataBinderFactory) {
+        String token = extractToken(nativeWebRequest)
+                .orElseThrow(() -> new InvalidTokenException("No token"));
+
+        provider.validateToken(token);
+        String email = provider.extractSubject(token);
         return postWriterService.retrievePostWriterBy(email);
+    }
+
+    private Optional<String> extractToken(NativeWebRequest nativeWebRequest) {
+        return Optional.ofNullable(nativeWebRequest
+                .getHeader(HttpHeaders.AUTHORIZATION));
     }
 }
