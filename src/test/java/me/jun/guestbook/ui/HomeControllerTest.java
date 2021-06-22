14a411b151eb4e1b0bfe48844a846b9272cc04a9
kjh9267 <intellijunho@naver.com>
2021-06-22 19:42:40 +0900

    refactor: Change read posts by page method

---
+++ src/test/java/me/jun/guestbook/ui/HomeControllerTest.java
@@ -0,0 +1,21 @@
+package me.jun.guestbook.ui;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+@Disabled
+public class HomeControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+}
