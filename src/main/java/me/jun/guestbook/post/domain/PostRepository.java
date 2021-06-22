2414169b79f8a7e25df7cf81e24f4bd75558b6a7
kjh9267 <kjh9267z@naver.com>
2021-06-22 19:42:40 +0900

    refactor: Change read posts by page method

--- src/main/java/me/jun/guestbook/post/domain/PostRepository.java
+++ src/main/java/me/jun/guestbook/post/domain/PostRepository.java
@@ -5,7 +5,4 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-
-    @Override
-    Page<Post> findAll(Pageable pageable);
 }
