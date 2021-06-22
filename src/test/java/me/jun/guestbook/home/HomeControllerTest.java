7ec872ec34299614a8f686db54b78743f436fff8
kjh9267 <intellijunho@kakao.com>
2021-06-22 19:47:42 +0900

    refactor: Move exception handler: ui -> common -refactor: Change package name: ui -> home

--- src/test/java/me/jun/guestbook/home/HomeControllerTest.java
+++ src/test/java/me/jun/guestbook/home/HomeControllerTest.java
@@ -1,11 +1,10 @@
-package me.jun.guestbook.ui;
+package me.jun.guestbook.home;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
