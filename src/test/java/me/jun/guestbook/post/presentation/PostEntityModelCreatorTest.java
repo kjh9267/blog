b4d3299feb644da33bc685aaf3aad19b98b8e6f6
kjh9267 <junhokim9267@kakao.com>
2021-06-22 23:33:52 +0900

    test: Create EntityModelCreator test

---
+++ src/test/java/me/jun/guestbook/post/presentation/PostEntityModelCreatorTest.java
@@ -0,0 +1,48 @@
+package me.jun.guestbook.post.presentation;
+
+import me.jun.guestbook.post.presentation.dto.PostResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.RepresentationModel;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class PostEntityModelCreatorTest {
+
+    private PostEntityModelCreator creator;
+
+    private PostResponse resource;
+
+    private Class controller;
+
+    @BeforeEach
+    void setUp() {
+        creator = new PostEntityModelCreator();
+
+        resource = PostResponse.builder()
+                .id(1L)
+                .title("test title")
+                .content("test content")
+                .build();
+
+        controller = PostController.class;
+    }
+
+    @Test
+    void createEntityModelTest() {
+        assertAll(
+                () -> assertThat(creator.createEntityModel(resource, controller))
+                        .isInstanceOf(EntityModel.class),
+                () -> assertThat(creator.createEntityModel(resource, controller).getContent())
+                        .isEqualToComparingFieldByField(resource)
+        );
+    }
+
+    @Test
+    void creatRepresentationModelTest() {
+        assertThat(creator.createEntityModel(controller))
+                .isInstanceOf(RepresentationModel.class);
+    }
+}
