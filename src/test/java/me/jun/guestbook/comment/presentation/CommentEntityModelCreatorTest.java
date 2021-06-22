239c4c9ba85d0b43d6fdb8ca2508d359949db235
kjh9267 <intellijunho@kakao.com>
2021-06-22 23:33:52 +0900

    test: Create EntityModelCreator test

---
+++ src/test/java/me/jun/guestbook/comment/presentation/CommentEntityModelCreatorTest.java
@@ -0,0 +1,52 @@
+package me.jun.guestbook.comment.presentation;
+
+import me.jun.guestbook.comment.presentation.dto.CommentResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.RepresentationModel;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@ExtendWith(MockitoExtension.class)
+class CommentEntityModelCreatorTest {
+
+    private CommentEntityModelCreator creator;
+
+    private CommentResponse resource;
+
+    private Class controller;
+
+    @BeforeEach
+    void setUp() {
+        creator = new CommentEntityModelCreator();
+
+        resource = CommentResponse.builder()
+                .id(1L)
+                .postId(1L)
+                .writerId(1L)
+                .content("test content")
+                .build();
+
+        controller = CommentController.class;
+    }
+
+    @Test
+    void createEntityModelTest() {
+        assertAll(
+                () -> assertThat(creator.createEntityModel(resource, controller))
+                        .isInstanceOf(EntityModel.class),
+                () -> assertThat(creator.createEntityModel(resource, controller).getContent())
+                        .isEqualToComparingFieldByField(resource)
+        );
+    }
+
+    @Test
+    void creatRepresentationModelTest() {
+        assertThat(creator.createEntityModel(controller))
+                .isInstanceOf(RepresentationModel.class);
+    }
+}
