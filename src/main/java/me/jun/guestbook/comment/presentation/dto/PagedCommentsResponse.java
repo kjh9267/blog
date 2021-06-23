a5cd825028492bfec7440f0d07e0f4459ca585f0
kjh9267 <intellijunho@naver.com>
2021-06-23 19:30:58 +0900

    feat: Create query comments by postid

---
+++ src/main/java/me/jun/guestbook/comment/presentation/dto/PagedCommentsResponse.java
@@ -0,0 +1,20 @@
+package me.jun.guestbook.comment.presentation.dto;
+
+import lombok.*;
+import me.jun.guestbook.comment.domain.Comment;
+import org.springframework.data.domain.Page;
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class PagedCommentsResponse {
+
+    private Page<CommentResponse> commentResponses;
+
+    public static PagedCommentsResponse from(Page<Comment> comments) {
+        return PagedCommentsResponse.builder()
+                .commentResponses(comments.map(CommentResponse::from))
+                .build();
+    }
+}
