0c7335b0fef1e25b893fcbfea9dcef29bd8d5db4
kjh9267 <junhokim9267@kakao.com>
2021-06-25 22:08:40 +0900

    feat: Create deleteGuest in GuestService - test: Create deleteGuestTest in GuestServiceTest - feat: Create deletePostByWriterId in PostService - test: Create deletePostByWriterIdTest in PostServiceTest - feat: Create delete deleteCommentByWriterId in CommentService - test: Create delete deleteCommentByWriterIdTest in CommentServiceTest - feat: Create deleteByWriterId in PostRepository - test: Create deleteByWriterIdTest in PostRepositoryTest - feat : Create deleteByWriterId in CommemntRepository - test : Create deleteByWriterIdTest in CommemntRepositoryTest

--- src/main/java/me/jun/guestbook/post/domain/PostRepository.java
+++ src/main/java/me/jun/guestbook/post/domain/PostRepository.java
@@ -1,8 +1,7 @@
 package me.jun.guestbook.post.domain;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    void deleteByWriterId(Long writerId);
 }
