853cbac681ca2158d6dfbd18ec6f3ed39c9300fb
kjh9267 <kjh9267z@naver.com>
2021-06-25 22:08:40 +0900

    feat: Create deleteGuest in GuestService - test: Create deleteGuestTest in GuestServiceTest - feat: Create deletePostByWriterId in PostService - test: Create deletePostByWriterIdTest in PostServiceTest - feat: Create delete deleteCommentByWriterId in CommentService - test: Create delete deleteCommentByWriterIdTest in CommentServiceTest - feat: Create deleteByWriterId in PostRepository - test: Create deleteByWriterIdTest in PostRepositoryTest - feat : Create deleteByWriterId in CommemntRepository - test : Create deleteByWriterIdTest in CommemntRepositoryTest

--- src/main/java/me/jun/guestbook/guest/application/GuestService.java
+++ src/main/java/me/jun/guestbook/guest/application/GuestService.java
@@ -1,10 +1,12 @@
 package me.jun.guestbook.guest.application;
 
 import lombok.RequiredArgsConstructor;
+import me.jun.guestbook.comment.application.CommentService;
 import me.jun.guestbook.guest.application.exception.GuestNotFoundException;
 import me.jun.guestbook.guest.domain.Guest;
 import me.jun.guestbook.guest.domain.GuestRepository;
 import me.jun.guestbook.guest.presentation.dto.GuestResponse;
+import me.jun.guestbook.post.application.PostService;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,10 +15,20 @@ public class GuestService {
 
     private final GuestRepository guestRepository;
 
+    private final PostService postService;
+
+    private final CommentService commentService;
+
     public GuestResponse retrieveGuestBy(String email) {
         Guest guest = guestRepository.findByEmail(email)
                 .orElseThrow(() -> new GuestNotFoundException());
 
         return GuestResponse.from(guest);
+    }
+
+    public void deleteGuest(Long guestId) {
+        guestRepository.deleteById(guestId);
+        postService.deletePostByWriterId(guestId);
+        commentService.deleteCommentByWriterId(guestId);
     }
 }
