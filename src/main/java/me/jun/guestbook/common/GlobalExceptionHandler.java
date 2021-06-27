d25352da630ad0b01629b338d245444456aaeca8
kjh9267 <intellijunho@kakao.com>
2021-06-27 20:33:47 +0900

    -feat: Create handler for invalid input

--- src/main/java/me/jun/guestbook/common/GlobalExceptionHandler.java
+++ src/main/java/me/jun/guestbook/common/GlobalExceptionHandler.java
@@ -1,44 +1,53 @@
 package me.jun.guestbook.common;
 
-import me.jun.guestbook.post.application.exception.WriterMismatchException;
-import me.jun.guestbook.post.application.exception.PostNotFoundException;
-import me.jun.guestbook.security.InvalidTokenException;
+import lombok.RequiredArgsConstructor;
 import me.jun.guestbook.common.error.ErrorCode;
 import me.jun.guestbook.common.error.ErrorResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import me.jun.guestbook.post.application.exception.PostNotFoundException;
+import me.jun.guestbook.post.application.exception.WriterMismatchException;
+import me.jun.guestbook.security.InvalidTokenException;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(BindException.class)
-    public ResponseEntity<ErrorResponse> bindExceptionHandler(BindException e) {
-        Logger logger = LoggerFactory.getLogger("empty input error logger");
-        logger.info("empty input");
+    private final ErrorEntityModelCreator errorEntityModelCreator;
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<EntityModel<ErrorResponse>>
+    methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
         ErrorResponse errorResponse = ErrorResponse.from(ErrorCode.EMPTY_VALUE);
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorEntityModelCreator.createErrorEntityModel(errorResponse),
+                HttpStatus.valueOf(errorResponse.getStatusCode()));
     }
 
     @ExceptionHandler(PostNotFoundException.class)
-    public ResponseEntity<ErrorResponse> postNotFoundExceptionHandler(PostNotFoundException e) {
+    public ResponseEntity<EntityModel<ErrorResponse>>
+    postNotFoundExceptionHandler(PostNotFoundException e) {
         ErrorResponse errorResponse = ErrorResponse.from(ErrorCode.POST_NOT_FOUND);
-        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorResponse.getStatusCode()));
+        return new ResponseEntity<>(errorEntityModelCreator.createErrorEntityModel(errorResponse),
+                HttpStatus.valueOf(errorResponse.getStatusCode()));
     }
 
     @ExceptionHandler(WriterMismatchException.class)
-    public ResponseEntity<ErrorResponse> writerMismatchExceptionHandler(WriterMismatchException e) {
+    public ResponseEntity<EntityModel<ErrorResponse>>
+    writerMismatchExceptionHandler(WriterMismatchException e) {
         ErrorResponse errorResponse = ErrorResponse.from(ErrorCode.GUEST_MISMATCH);
-        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorResponse.getStatusCode()));
+        return new ResponseEntity<>(errorEntityModelCreator.createErrorEntityModel(errorResponse),
+                HttpStatus.valueOf(errorResponse.getStatusCode()));
     }
 
     @ExceptionHandler(InvalidTokenException.class)
-    public ResponseEntity<ErrorResponse> NoTokenExceptionHandler(InvalidTokenException e) {
+    public ResponseEntity<EntityModel<ErrorResponse>>
+    NoTokenExceptionHandler(InvalidTokenException e) {
         ErrorResponse errorResponse = ErrorResponse.from(ErrorCode.UNAUTHORIZED);
-        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorResponse.getStatusCode()));
+        return new ResponseEntity<>(errorEntityModelCreator.createErrorEntityModel(errorResponse),
+                HttpStatus.valueOf(errorResponse.getStatusCode()));
     }
 }
