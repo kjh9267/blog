f081c1e8392547025feab0f7cb208778b98af7d9
kjh9267 <intellijunho@kakao.com>
2021-07-18 15:50:04 +0900

    test: Organize guest test fixtures - feat: Create GuestFixture - refactor: delete guest fixtures in GuestService, LoginService, RegisterService, GuestController

--- src/test/java/me/jun/guestbook/guest/application/LoginServiceTest.java
+++ src/test/java/me/jun/guestbook/guest/application/LoginServiceTest.java
@@ -1,11 +1,6 @@
 package me.jun.guestbook.guest.application;
 
-import me.jun.guestbook.guest.presentation.dto.GuestRequest;
-import me.jun.guestbook.guest.presentation.dto.GuestResponse;
-import me.jun.guestbook.guest.presentation.dto.TokenResponse;
-import me.jun.guestbook.guest.application.exception.DuplicatedEmailException;
 import me.jun.guestbook.guest.application.exception.EmailNotFoundException;
-import me.jun.guestbook.guest.domain.Guest;
 import me.jun.guestbook.guest.domain.GuestRepository;
 import me.jun.guestbook.security.JwtProvider;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,12 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.Optional;
 
+import static me.jun.guestbook.guest.GuestFixture.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -34,48 +28,26 @@ public class LoginServiceTest {
     @Mock
     private JwtProvider jwtProvider;
 
-    private Guest guest;
-
-    private GuestRequest guestRequest;
-
-    private final String name = "testuser";
-
-    private final String email = "testuser@email.com";
-
-    private final String password = "pass";
-
     @BeforeEach
     public void setUp() {
         loginService = new LoginService(guestRepository, jwtProvider);
-
-        guest = Guest.builder()
-                .name(name)
-                .email(email)
-                .password(password)
-                .build();
-
-        guestRequest = GuestRequest.builder()
-                .name(name)
-                .email(email)
-                .password(password)
-                .build();
     }
 
     @Test
     void loginTest() {
-        given(jwtProvider.createJwt(any())).willReturn("1.2.3");
-        given(guestRepository.findByEmail(any())).willReturn(Optional.of(guest));
+        given(jwtProvider.createJwt(any())).willReturn(JWT);
+        given(guestRepository.findByEmail(any())).willReturn(Optional.of(guest()));
 
-        assertThat(loginService.login(guestRequest))
-                .isEqualTo(TokenResponse.from("1.2.3"));
+        assertThat(loginService.login(guestRequest()))
+                .isEqualTo(tokenResponse());
     }
 
     @Test
     void NoGuest_loginFailTest() {
-        given(guestRepository.findByEmail(email)).willReturn(Optional.empty());
+        given(guestRepository.findByEmail(EMAIL)).willReturn(Optional.empty());
 
         assertThrows(EmailNotFoundException.class,
-                () -> loginService.login(guestRequest)
+                () -> loginService.login(guestRequest())
         );
     }
 }
