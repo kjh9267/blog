76abeef6a07eaa31ac25ada67a0655ff3770692a
kjh9267 <intellijunho@naver.com>
2021-07-18 16:43:53 +0900

    test: Organize comment test fixtures - feat: Create CommentFixture - refactor: delete comment fixtures in CommentService, CommentController

--- src/test/java/me/jun/guestbook/comment/domain/CommentTest.java
+++ src/test/java/me/jun/guestbook/comment/domain/CommentTest.java
@@ -2,19 +2,23 @@ package me.jun.guestbook.comment.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static me.jun.guestbook.comment.CommentFixture.comment;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class CommentTest {
 
     @Test
     void constructorTest() {
-        Comment comment = Comment.builder()
+        Comment expected = Comment.builder()
                 .id(1L)
                 .postId(1L)
+                .writerId(1L)
                 .content("test content")
                 .build();
 
-        assertThat(comment).isInstanceOf(Comment.class);
+        assertAll(() -> assertThat(comment()).isInstanceOf(Comment.class),
+                () -> assertThat(comment()).isEqualToComparingFieldByField(expected)
+        );
     }
 }
