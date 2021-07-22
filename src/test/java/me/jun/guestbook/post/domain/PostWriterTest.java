cac30365daebb229cc7ef5895652fddb23b32941
kjh9267 <intellijunho@naver.com>
2021-07-22 23:44:45 +0900

    refactor: Change deletePostByWriterId -> deletePostByPostWriter in PostRepository - test: Change Change deletePostByWriterIdTest -> deletePostByPostWriterTest in PostRepository - test: Create validateWriter Test - test: Create match Test - style: WriterMismatchException -> PostWriterMismatchException

---
+++ src/test/java/me/jun/guestbook/post/domain/PostWriterTest.java
@@ -0,0 +1,33 @@
+package me.jun.guestbook.post.domain;
+
+import me.jun.guestbook.comment.domain.PostWriter;
+import org.junit.jupiter.api.Test;
+
+import static me.jun.guestbook.post.PostFixture.postWriter;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class PostWriterTest {
+
+    @Test
+    void constructorTest() {
+        PostWriter postWriter = postWriter();
+
+        PostWriter expected = new PostWriter(1L);
+
+        assertAll(
+                () -> assertThat(postWriter).isInstanceOf(PostWriter.class),
+                () -> assertThat(postWriter).isEqualToComparingFieldByField(expected)
+        );
+    }
+
+    @Test
+    void matchTest() {
+        PostWriter postWriter = postWriter();
+
+        assertAll(
+                () -> assertThat(postWriter.match(1L)).isTrue(),
+                () -> assertThat(postWriter.match(2L)).isFalse()
+        );
+    }
+}
