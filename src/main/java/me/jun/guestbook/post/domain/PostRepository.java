10b35e1beaa5e84b5b4bd683e3aa45ee31e22603
kjh9267 <kjh9267z@naver.com>
2021-08-31 18:46:20 +0900

    fix: Change PostWriter package Comment -> Post

--- src/main/java/me/jun/guestbook/post/domain/PostRepository.java
+++ src/main/java/me/jun/guestbook/post/domain/PostRepository.java
@@ -1,6 +1,6 @@
 package me.jun.guestbook.post.domain;
 
-import me.jun.guestbook.comment.domain.PostWriter;
+import me.jun.guestbook.post.domain.PostWriter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
