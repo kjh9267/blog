809e5ffe7e790a7eda295f9f48770ccd9bd92fcd
kjh9267 <intellijunho@naver.com>
2021-08-31 18:07:47 +0900

    test: Create leave REST API test -refactor: Change GuestResolver return GuestInfo -fix: Add GuestResolver in MvcConfig

--- src/main/java/me/jun/guestbook/config/MvcConfig.java
+++ src/main/java/me/jun/guestbook/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package me.jun.guestbook.config;
 
 import lombok.RequiredArgsConstructor;
+import me.jun.guestbook.guest.presentation.GuestResolver;
 import me.jun.guestbook.post.presentation.CommentWriterResolver;
 import me.jun.guestbook.post.presentation.PostWriterResolver;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +18,12 @@ public class MvcConfig implements WebMvcConfigurer {
 
     private final CommentWriterResolver commentWriterResolver;
 
+    private final GuestResolver guestResolver;
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(postWriterResolver);
         resolvers.add(commentWriterResolver);
+        resolvers.add(guestResolver);
     }
-
 }
