ef3de0bc76c7929b27f9ad8c283f8435ca8fe94c
kjh9267 <intellijunho@naver.com>
2021-10-01 21:16:32 +0900

    refactor: Change GuestEntityModelCreator implements hateoas interfaces

--- src/main/java/me/jun/guestbook/guest/presentation/GuestEntityModelCreator.java
+++ src/main/java/me/jun/guestbook/guest/presentation/GuestEntityModelCreator.java
@@ -1,6 +1,8 @@
 package me.jun.guestbook.guest.presentation;
 
 import me.jun.guestbook.guest.application.dto.TokenResponse;
+import me.jun.guestbook.hateoas.EntityModelCreator;
+import me.jun.guestbook.hateoas.HyperlinksCreator;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.stereotype.Component;
@@ -9,24 +11,22 @@ import static me.jun.guestbook.support.RelUtils.*;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @Component
-public class GuestEntityModelCreator {
+public class GuestEntityModelCreator implements HyperlinksCreator, EntityModelCreator<TokenResponse> {
+
     private final Class<GuestController> controller = GuestController.class;
 
-    EntityModel<TokenResponse> createLoginEntityModel(TokenResponse tokenResponse) {
+    @Override
+    public EntityModel<TokenResponse> createEntityModel(TokenResponse tokenResponse) {
         return EntityModel.of(tokenResponse)
                 .add(linkTo(controller).slash(LOGIN).withSelfRel())
                 .add(linkTo(controller).slash(REGISTER).withRel(REGISTER));
     }
 
-    RepresentationModel createRegisterRepresentationModel() {
+    @Override
+    public RepresentationModel createHyperlinks() {
         return new RepresentationModel()
-                .add(linkTo(controller).slash(REGISTER).withSelfRel())
-                .add(linkTo(controller).slash(LOGIN).withRel(LOGIN));
-    }
-
-    RepresentationModel createLeaveRepresentationModel() {
-        return new RepresentationModel()
-                .add(linkTo(controller).slash(LEAVE).withSelfRel())
+                .add(linkTo(controller).slash(LEAVE).withRel(LEAVE))
+                .add(linkTo(controller).slash(LOGIN).withRel(LOGIN))
                 .add(linkTo(controller).slash(REGISTER).withRel(REGISTER));
     }
 }
