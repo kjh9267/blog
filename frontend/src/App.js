0eb2c326f8003aa90e01b4352117f8be2e2fa2eb
kjh9267 <kjh9267z@naver.com>
2021-11-11 19:32:27 +0900

    feat: Create main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useCookies } from 'react-cookie';
 
 function App() {
+  // axios.defaults.withCredentials = true;
+
+  let [cookies, setCookie] = useCookies(['access_token']);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => {
+        // axios.defaults.withCredentials = true;
+        axios.get("http://localhost:8080/api/posts/query?page=0&size=10")
+        .then((error) => console.log(error));
+      }}>
+      getlist
+      </button>
+
+      <div>
+      <button onClick={() => {
+        // axios.defaults.withCredentials = true;
+        axios.post("http://localhost:8080/api/register",
+          {'email': 'testuser@email.com',
+          'name': 'testuser',
+          'password': 'pass'
+        }
+        )
+        .then((error) => console.log(error));
+      }}>
+      register
+      </button>
+      </div>
+
+      <div>
+      <button onClick={() => {
+        axios.post("http://localhost:8080/api/login",
+          {'email': 'testuser@email.com',
+          'name': 'testuser',
+          'password': 'pass'
+        })
+        .then((response) => {
+          if(response.status === 200) {
+            setCookie('access_token', response.data['access_token']);
+          }
+          console.log(cookies);
+          console.log(response.data['access_token']);
+        });
+      }}>
+      login
+      </button>
+      </div>
     </div>
   );
 }
