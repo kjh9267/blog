cc0890854e49ad44cc7da73fa8efa3cb4d06978f
kjh9267 <intellijunho@kakao.com>
2021-11-17 20:30:25 +0900

    feat: Create version for concurrent update -feat: Create retry for version mismatch

--- src/main/java/me/jun/Application.java
+++ src/main/java/me/jun/Application.java
@@ -2,9 +2,11 @@ package me.jun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableRetry
 public class Application {
 
     public static void main(String[] args) {
