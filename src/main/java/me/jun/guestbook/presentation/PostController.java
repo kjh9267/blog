0cab2e2302f6463eec8d8a06cf18fd272590cef8
kjh9267 <kjh9267z@naver.com>
2021-11-26 21:52:38 +0900

    refactor: Change post controller to reactive

--- src/main/java/me/jun/guestbook/presentation/PostController.java
+++ src/main/java/me/jun/guestbook/presentation/PostController.java
@@ -1,15 +1,19 @@
 package me.jun.guestbook.presentation;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.jun.guestbook.application.PostService;
 import me.jun.guestbook.application.dto.*;
+import me.jun.guestbook.domain.Post;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
-import java.net.URI;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/posts")
@@ -18,48 +22,71 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping
-    public ResponseEntity<PostResponse> createPost(@RequestBody @Valid PostCreateRequest request,
-                                                   @PostWriter PostWriterInfo writer) {
-        PostResponse postResponse = postService.createPost(request, writer.getId());
+    public ResponseEntity<Mono<PostResponse>> createPost(@RequestBody @Valid PostCreateRequest request,
+                                                        @PostWriter PostWriterInfo writer) {
 
-        URI location = URI.create("/api/posts" + postResponse.getId());
+        Mono<PostResponse> postResponseMono = Mono.fromCompletionStage(
+                () -> postService.createPost(request, writer.getId())
+        ).log();
 
-        return ResponseEntity.created(location)
-                .body(postResponse);
+        log.info("end");
+
+        return ResponseEntity.ok()
+                .body(postResponseMono);
     }
 
     @GetMapping("/{postId}")
-    public ResponseEntity<PostResponse> retrievePost(@PathVariable Long postId) {
-        PostResponse postResponse = postService.retrievePost(postId);
+    public ResponseEntity<Mono<PostResponse>> retrievePost(@PathVariable Long postId) {
+
+        Mono<PostResponse> postResponseMono = Mono.fromCompletionStage(
+                () -> postService.retrievePost(postId)
+        ).log();
+
+        log.info("end");
 
         return ResponseEntity.ok()
-                .body(postResponse);
+                .body(postResponseMono);
     }
 
     @PutMapping
-    public ResponseEntity<PostResponse>
-    updatePost(@RequestBody @Valid PostUpdateRequest requestDto,
-               @PostWriter PostWriterInfo writer) {
-        PostResponse postResponse = postService.updatePost(requestDto, writer.getId());
+    public ResponseEntity<Mono<PostResponse>> updatePost(@RequestBody @Valid PostUpdateRequest requestDto,
+                                                         @PostWriter PostWriterInfo writer) {
+
+        Mono<PostResponse> postResponseMono = Mono.fromCompletionStage(
+                () -> postService.updatePost(requestDto, writer.getId())
+        ).log();
+
+        log.info("end");
 
         return ResponseEntity.ok()
-                .body(postResponse);
+                .body(postResponseMono);
     }
 
     @DeleteMapping("/{postId}")
-    public ResponseEntity<Void> deletePost(@PathVariable Long postId,
-                                           @PostWriter PostWriterInfo writer) {
-        postService.deletePost(postId, writer.getId());
+    public ResponseEntity<Mono<Long>> deletePost(@PathVariable Long postId,
+                                                 @PostWriter PostWriterInfo writer) {
+        Mono<Long> mono = Mono.fromCompletionStage(
+                () -> postService.deletePost(postId, writer.getId())
+        ).log();
 
-        return ResponseEntity.ok().build();
+        log.info("end");
+
+        return ResponseEntity.ok()
+                .body(mono);
     }
 
     @GetMapping("/query")
-    ResponseEntity<PagedPostsResponse> queryPosts(@RequestParam("page") Integer page,
-                                            @RequestParam("size") Integer size) {
-        PagedPostsResponse response = postService.queryPosts(PageRequest.of(page, size));
+    ResponseEntity<Flux<PagedPostsResponse>> queryPosts(@RequestParam("page") Integer page,
+                                                        @RequestParam("size") Integer size) {
 
-        return ResponseEntity.ok()
-                .body(response);
+//        PagedPostsResponse response = postService.queryPosts(PageRequest.of(page, size));
+
+        log.info("end");
+
+        return null;
+
+
+//        return ResponseEntity.ok()
+//                .body(postFlux);
     }
 }
