9329c35ad08029f3bca1dfa091374e6d5cc01588
kjh9267 <kjh9267z@naver.com>
2021-12-04 20:12:49 +0900

    refactor: Change profiles

--- src/test/java/me/jun/member/presentation/MemberControllerTest.java
+++ src/test/java/me/jun/member/presentation/MemberControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static me.jun.member.MemberFixture.*;
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @SpringBootTest
 public class MemberControllerTest {
