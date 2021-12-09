520b680279e234010f8599745d0148c7e8860c9c
kjh9267 <junhokim9267@kakao.com>
2021-12-09 18:58:43 +0900

    feat: Create Register, Login Component

---
+++ frontend/src/components/Write.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+import { Cookies } from 'react-cookie';
+
+function Write() {
+    return(
+        <div>
+            {}
+
+        </div>
+    )
+}
+
+export default Write;
