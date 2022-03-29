cbd3d18d37319124c8bb3b384328f47abceb5f1e
kjh9267 <junhokim9267@kakao.com>
2022-03-29 16:51:32 +0900

    refactor: extract get api

--- frontend/src/blog/components/Register.js
+++ frontend/src/blog/components/Register.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 import axios from 'axios';
-import { useState } from 'react';
-import { REGISTER } from '../../support/UrlUtils';
+import {REGISTER} from '../../support/UrlUtils';
 
 
 function Register() {
