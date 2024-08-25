deee191ab64c9faf567093c0f76dcd2c42874d43
kjh9267 <intellijunho@kakao.com>
2024-08-26 07:12:04 +0900

    feat: Create CreateTagToArticleLock AOP -> concurrent create tag problem -test: Create TaggedArticleServiceTransactionTest

---
+++ src/test/java/me/jun/blog/application/TaggedArticleServiceTransactionTest.java
@@ -0,0 +1,47 @@
+package me.jun.blog.application;
+
+import me.jun.blog.application.dto.AddTagRequest;
+import me.jun.blog.domain.repository.TaggedArticleRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static me.jun.blog.TaggedArticleFixture.addTagRequest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+class TaggedArticleServiceTransactionTest {
+
+    @Autowired
+    private TaggedArticleService taggedArticleService;
+
+    @Autowired
+    private TaggedArticleRepository taggedArticleRepository;
+
+    @Test
+    void createTagToArticleTest() throws InterruptedException {
+        int expected = 2_000;
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (long thread = 0; thread < 1_000; thread++) {
+            for (int tagName = 0; tagName < 2; tagName++) {
+                AddTagRequest request = addTagRequest().toBuilder()
+                        .articleId(thread)
+                        .tagName(String.valueOf(tagName))
+                        .build();
+
+                executorService.submit(() -> taggedArticleService.createTagToArticle(request));
+            }
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(10, TimeUnit.MINUTES);
+
+        assertThat(taggedArticleRepository.findAll().size())
+                .isEqualTo(expected);
+    }
+}
